60b06155be9e082b3c6c7c0fe4021ec6e6a9ed7a
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2025-01-04 02:33:59 +0700

    Menambahkan comment dan juga menambahkan sebuah fungsi

--- NewProject.py
+++ NewProject.py
@@ -1,2 +1,9 @@
 print("hello world!")
-# comment di python
+# comment di python.
+name = input("Masukan nama anda!")
+print (name)
+#diatas adalah perintah untuk mengambil input user pada python.
+
+'''
+ini adalah block comment
+'''
