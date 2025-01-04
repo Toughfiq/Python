acf00bff7c8f8adebb4cc3043a5f57f5fb9776f7
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2025-01-05 03:06:15 +0700

    multi line string

--- NewProject.py
+++ NewProject.py
@@ -15,3 +15,12 @@ print (var)
 print (id(var))
 
 #hal diatas membutikan bahwa numbers di python bersifat immutable karena address yang dihasilkan
+# saat assignment ulang dilakukan maka bukan merubah isi dari variable itu sendiri tetapi menulis ulang variable tersebut.
+
+Line2 = """
+ini adalah string dengan banyak baris
+dengan menggunakan triple quote
+ """
+print(Line2)
+
+# string di python bisa multi line dengan menggunakan triple quote
