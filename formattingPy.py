bd9e7db17fee3e1591f17162818d4641aaa72b82
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2025-01-09 22:26:26 +0700

    Menambah contoh zfill

--- formattingPy.py
+++ formattingPy.py
@@ -1,4 +1,14 @@
 # zfill()
+'''
+zfill berguna untuk menambahkan
+nilai nol (0) di depan sebuah string dengan panjang tertentu
+biasa digunakan untuk merapikan nomor sehingga memiliki format yang konsisten.
+'''
+new = "13"
+tambahZ = new.zfill(5)
+print(tambahZ)
+# zfill ini tidak bisa menggunakan tipe data int
+
 #rjust()
 #ljust()
 #center() 
