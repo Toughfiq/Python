d32dffe052f442997ca9d8230dbc77b949a54baf
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2025-01-17 11:58:43 +0700

    update Condition.py

--- Condition.py
+++ Condition.py
@@ -1,2 +1,7 @@
 name = input ("sebutkan nama :")
 print(name)
+try:
+    nilai = int(input(f"{name}, masukkan nilai: "))
+    print(nilai)
+except ValueError:
+    print("Input harus berupa angka!")
