b5908beee540005f104f6566dcb14bad1d4861a2
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2025-01-17 12:02:56 +0700

    update Condition.py

--- Condition.py
+++ Condition.py
@@ -5,3 +5,14 @@ try:
     print(nilai)
 except ValueError:
     print("Input harus berupa angka!")
+nilai = 75
+if nilai >= 85:
+    print("Nilai A,Hebat")
+elif nilai >= 70:
+    print("Nilai B,Tingkatkan lagi")
+elif nilai>=60
+  print("Nilai C,Belajar lagi ya!")
+elif nilai>=50
+  print("Nilai D,Jangan menyerah")
+else:
+    print("Nilai E,Goblok!")
