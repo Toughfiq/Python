73aa777d2d540549449ae225d0ab3e2d515abb0f
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2025-01-19 13:41:53 +0700

    update looping.py

--- looping.py
+++ looping.py
@@ -1,9 +1,11 @@
-#for loop
+#=========for loop========
 buah = ["jambu", "buni", "tamarin", "menteng", "matoa"]
 for b in buah:
-  print(buah)
+  print(b)
   """
   b adalah sebuah variable sementara untuk menyimpan file iterable/file yang dapat dihitung,dalam kasus diatas berjumlah 5, b dapat diganri dengan sembarang variable lain.
-  
+  jika dilakukan print(buah) maka output yang akan dihasilkan adalah full list sebanyak jumlah file iterable atau full list sebanyak 5 kali
   """
+  #====== for loop dengan range ======
+  
 #while loop
