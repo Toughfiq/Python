1b1b739ed878ac3621ab2ac6f614e8afb6a474a4
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2025-01-19 13:39:43 +0700

    update looping.py

--- looping.py
+++ looping.py
@@ -2,4 +2,8 @@
 buah = ["jambu", "buni", "tamarin", "menteng", "matoa"]
 for b in buah:
   print(buah)
+  """
+  b adalah sebuah variable sementara untuk menyimpan file iterable/file yang dapat dihitung,dalam kasus diatas berjumlah 5, b dapat diganri dengan sembarang variable lain.
+  
+  """
 #while loop
