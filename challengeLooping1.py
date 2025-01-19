a42e500e587402226e595f9b63478714c365ccd0
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2025-01-20 03:05:30 +0700

    challenge

---
+++ challengeLooping1.py
@@ -0,0 +1,13 @@
+'''
+Sebuah peternakan ayam memiliki sistem conveyor belt yang mengangkut telur untuk diperiksa. 
+Sistem ini perlu memeriksa telur satu per satu sampai menemukan telur yang retak atau 
+sampai semua telur sudah diperiksa.
+Buatlah program yang:
+
+Menerima list berisi kondisi telur (0 untuk telur bagus, 1 untuk telur retak)
+Memeriksa telur satu per satu menggunakan for loop
+Jika menemukan telur retak (1), program harus berhenti dan memberitahu posisi 
+telur yang retak
+Jika semua telur sudah diperiksa dan tidak ada yang retak, program memberitahu 
+bahwa semua telur dalam kondisi baik
+'''
