3489ce1ef02e64f346f3d47de2ba610c3cd325ed
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2025-01-22 01:59:14 +0700

    Menambah beberapa baris array dan juga list untuk membandingkan

--- Arrlearn.py
+++ Arrlearn.py
@@ -1,1 +1,12 @@
+import array
 #kita akan membahas array pada kesempatan kali ini 
+'''
+Array paling wajar dan mudah ditemui pada python adalah berbentuk list
+namun penggunaan module array akan membuat lebih efisien dalam hal memory
+'''
+arr1 = array.array('i', [1, 2, 4, 7, 14, 56, 128])
+print (arr1)
+print (id(arr1[3]))
+listy = [1, 2, 4, 7, 14, 56, 128,]
+print (listy)
+print(id(listy[2]))
