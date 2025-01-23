9db633a7ed2f51226732af904b63df794175e574
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2025-01-23 13:45:37 +0700

    Memperbaiki code agar lebih muda dibaca

--- learnNumpy.py
+++ learnNumpy.py
@@ -2,26 +2,28 @@
 import numpy as np
 
 # Membuat matriks 3x3
+import numpy as np
+
 A = np.array([
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ])
-
+#Print matriks A
+print(f"ini adalah matriks A = \n", A)
 # Akses elemen (baris ke-2, kolom ke-3)
-print(A[1, 2])  # Output: 6
-
-# Transpos matriks
-print(f"ini adalah transpose matrikas", "\n", A.T)
-
+print(f"elemen di indeks 1,2 = \n", A[1, 2])  # Output: 6
+# Transpose matriks
+print(f"Ini adalah hasi dari transpose matriks =\n", A.T)
 # Menjumlahkan matriks
 B = np.array([
     [9, 8, 7],
     [6, 5, 4],
     [3, 2, 1]
 ])
+print(f"ini adalah matriks B = \n", B)
 C = A + B
-print(C)
+print(f"ini adalah hasil penjumlahan matriks = \n", C)
 '''
 Beberapa operasi penting dalam NumPy:
 
