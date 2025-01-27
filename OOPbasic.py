16ff5c32cbff7fa2130ee3766f2f8c5b4240ce3e
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2025-01-27 23:59:12 +0700

    menambah bagaimana oop bekerja secara dasar

--- OOPbasic.py
+++ OOPbasic.py
@@ -5,5 +5,9 @@ mobil = "bergerak maju"
 
 class Mobil:
     def __init__(self, warna, bergerak):
+        # class mobil akan memiliki constructor init menerima 2 parameter
         self.warna = warna
-        self.bergerak = bergerak    
+        self.bergerak = bergerak #warna dan bergerak ini adalah instance    
+suzuki = Mobil("Navy", "maju")
+
+print(suzuki.warna)
