6d9d515ad4eb55d23ce5da038407e7ad7ed21c86
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2025-01-27 23:49:25 +0700

    menambahkan class

--- OOPbasic.py
+++ OOPbasic.py
@@ -2,3 +2,8 @@
 mobil = "bergerak maju"
 #mobil dapat di analogi kan sebagai class
 #Sementara bergerak maju adalah method 
+
+class Mobil:
+    def __init__(self, warna, bergerak):
+        self.warna = warna
+        self.bergerak = bergerak    
